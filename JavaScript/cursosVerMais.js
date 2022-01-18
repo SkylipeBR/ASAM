45d6d3a80973f288429066a37392103e4f824fc5
Filipe Angelo <75441666+SkylipeBR@users.noreply.github.com>
2022-01-18 13:58:52 -0300

    Btn ver mais
    BTN ver mais está terminado, inddo e voltando com Ver menos

--- JavaScript/cursosVerMais.js
+++ JavaScript/cursosVerMais.js
@@ -4,4 +4,19 @@ function btncurso()
             curso.classList.remove("desativado");
         });
 
+
+        // $("#btn-ver-mais").click(mostraMais);
+
+        // function mostraMais() {
+        // $(".texto").toggle();
+        // };
+
+        if($("#btn-ver-mais").text() === 'Voltar') {
+            $("#btn-ver-mais").text('Ver mais')
+            document.querySelectorAll("#curso").forEach(curso => {
+                curso.classList.add("desativado");
+            });
+          } else {
+            $("#btn-ver-mais").text('Voltar')
+          }
 } 
